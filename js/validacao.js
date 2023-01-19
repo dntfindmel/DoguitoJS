bd8c343b4442159b4294364e04e8f3c051007b31
Melyssa Moya <120610288+dntfindmel@users.noreply.github.com>
2023-01-18 21:04:16 -0300

    Form validation

---
+++ js/validacao.js
@@ -0,0 +1,23 @@
+const dataNascimento = document.querySelector('#nascimento')
+
+dataNascimento.addEventListener('load', (evento) => {
+     validacaoDataNascimento(evento.target)
+})
+
+function validacaoDataNascimento(input) {
+    const dataRecebida = new Date(input.value)
+    let mensagem = ''
+
+    if (!maiorDe18(dataRecebida)) {
+        mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'
+    }
+
+    input.setCustomValidity(mensagem)
+}
+
+function maiorDe18(data) {
+    const dataAtual = new Date()
+    const dataMais18 = new Date (data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
+
+    return dataMais18 <= dataAtual
+}
